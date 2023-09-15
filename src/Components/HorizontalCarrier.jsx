c851278ac4867dd87cc4129298d6a352cbe52a19
ariasmarmando <115955289+ariasmarmando@users.noreply.github.com>
2023-09-14 23:23:04 -0400

    scroll logic created for main content placeholders

--- src/Components/HorizontalCarrier.jsx
+++ src/Components/HorizontalCarrier.jsx
@@ -2,10 +2,10 @@ import "../CSS/HCarrier.css";
 const HorizontalCarrier = () => {
   return (
     <div className="elements-carrier">
-      <div id="firstObj" className="spaceBlock"></div>
-      <div id="secondObj" className="spaceBlock"></div>
-      <div id="thirdObj" className="spaceBlock"></div>
-      <div id="fourthObj" className="spaceBlock"></div>
+      <div id="home" className="spaceBlock"></div>
+      <div id="aboutMe" className="spaceBlock"></div>
+      <div id="projects" className="spaceBlock"></div>
+      <div id="contact" className="spaceBlock"></div>
     </div>
   );
 };
