7ca214ad26226a595ec9c17fc7f00c7ce50b6f01
ariasmarmando <115955289+ariasmarmando@users.noreply.github.com>
2023-09-15 23:02:26 -0400

    id card finished. certain bugs fixed. image added. data switched from fixed data to props

--- src/Components/HorizontalCarrier.jsx
+++ src/Components/HorizontalCarrier.jsx
@@ -1,11 +1,24 @@
 import "../CSS/HCarrier.css";
 import IDCard from "./IDCard";
+import IDimage from "../images/PFP3.png";
+
+const personalData = {
+  pfp: IDimage,
+  name: "Armando Arias",
+  title1: "Front-End Developer",
+  title2: "UX / UI Designer",
+};
 
 const HorizontalCarrier = () => {
   return (
     <div className="elements-carrier">
       <div id="home" className="spaceBlock">
-        <IDCard />
+        <IDCard
+          pfp={personalData.pfp}
+          name={personalData.name}
+          title1={personalData.title1}
+          title2={personalData.title2}
+        />
       </div>
       <div id="aboutMe" className="spaceBlock"></div>
       <div id="projects" className="spaceBlock"></div>
