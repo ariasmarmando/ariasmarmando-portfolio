164fa92efb81ded3716044f3450fb193adf072db
ariasmarmando <115955289+ariasmarmando@users.noreply.github.com>
2023-09-20 22:24:39 -0400

    title and bio created and styled

--- src/Components/HorizontalCarrier.jsx
+++ src/Components/HorizontalCarrier.jsx
@@ -32,7 +32,22 @@ const HorizontalCarrier = (props) => {
           title1={props.title1}
           title2={props.title2}
         />
+        <div className="bio-section">
+          <div id="bio-title1">
+            <span id="bio-slash">// </span>
+            <span id="bio-title">Get to know me</span>
+          </div>
+          <span id="bio-bio">
+            Programming since the age of 15, when I first fell in love with the
+            freedom that one obtains when you learn to speak the language of
+            computers and the web. I am a Front-End certified web developer,
+            currently pursuing a Bachelor's in Computer Science. I enjoy
+            collaborating with teammates to develop stunning and accessible
+            solutions.
+          </span>
+        </div>
       </div>
+
       <div id="projects" className="spaceBlock"></div>
       <div id="contact" className="spaceBlock"></div>
     </div>
